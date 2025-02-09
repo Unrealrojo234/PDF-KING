aab8a26c20b374e98d3091429285c152fe1d3805
Unrealrojo234 <otienotyan812@gmail.com>
2025-02-09 14:10:54 +0300

    mmmh

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,6 +1,6 @@
 let sendBtn = document.querySelector("button");
 let promptTextarea = document.querySelector("textarea");
-let responseDiv = document.getElementById("response");
+let responseDiv = document.querySelector(".response");
 let loader = document.querySelector(".loader");
 
 let fileInput = document.getElementById("fileInput");
@@ -9,7 +9,17 @@ responseDiv.style.display = "none";
 
 loader.style.display = "none";
 
+document.getElementById("status").style.display = "none";
+
 let pdfExtract = null;
+
+//Variables for holding usage values
+let inputTokens = null;
+let outputTokens = null;
+let totalTokens = null;
+
+//Status codes
+let statusCode = null;
 
 const postFile = (formData) => {
   fetch("/api/upload", {
@@ -86,10 +96,40 @@ const postPrompt = (prompt) => {
         loader.style.display = "none";
 
         let res = data.output.choices[0].message.content;
-        console.log(res);
+
+        //Usage
+        inputTokens = data.usage.input_tokens;
+        outputTokens = data.usage.output_tokens;
+        totalTokens = data.usage.total_tokens;
+
+        //Status
+        statusCode = data.status_code;
+
+        // console.table({
+        //   input_tokens: inputTokens,
+        //   output_tokens: outputTokens,
+        //   total_tokens: totalTokens,
+        //   status_code: statusCode,
+        // });
 
         responseDiv.style.display = "block";
         responseDiv.innerHTML = res;
+
+        document.getElementById("inputTokens").innerText = inputTokens;
+        document.getElementById("outputTokens").innerText = outputTokens;
+        document.getElementById("totalTokens").innerText = totalTokens;
+
+        let statusCodeSpan = document.getElementById("statusCode");
+
+        document.getElementById("status").style.display = "block";
+
+        if (statusCode == 200) {
+          statusCodeSpan.style.color = "green";
+          statusCodeSpan.innerText = "Success!";
+        } else {
+          statusCodeSpan.style.color = "red";
+          statusCodeSpan.innerText = "Error!";
+        }
       })
       .catch((error) => {
         loader.style.display = "none";
